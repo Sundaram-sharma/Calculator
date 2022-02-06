1aab0023a5898fbf0d548b11703fe8ab14971c12
sundaramsharma <sundaramsharma1209@gmail.com>
2022-02-06 16:33:34 -0500

    updated UI 2

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+    fun onDigit(view: View){
+        tvInput.append("1")
+    }
+
 }
