962c76c1d1e0ee493d4773f4d1c79e9083ae5492
sundaramsharma <sundaramsharma1209@gmail.com>
2022-02-06 21:51:58 -0500

    updated logic 2

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
 
     var isLastNumeric: Boolean = false //tells weather the last number is numeric or not
 
@@ -36,6 +39,25 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+
+    }
+
+    fun onOperator(view: View){
+        Log.d("finder1","this is working")
+        if(isLastNumeric&&!isOperatorAdded(tvDisplay.text.toString())){
+
+            // enter the same value of the button
+                Log.d("finder2","this is working")
+            tvDisplay.append((view as Button).text)
+            Log.d("finder3","this is working")
+            isLastNumeric = false // update the value
+            isLastDecimal = false // update the value
+
+        }
+
+    }
+
+    fun onEqualsTo(view: View){
 
     }
 
